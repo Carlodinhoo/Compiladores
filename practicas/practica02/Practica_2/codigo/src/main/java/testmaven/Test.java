3e6fb8ea91719b9013dd1573be710d497ae23543
Héctor Santaella Marín <santaella@ciencias.unam.mx>
2018-05-22 22:02:21 -0500

    Se regreso al estado normal la práctica 2

--- practicas/practica02/Practica_2/codigo/src/main/java/testmaven/Test.java
+++ practicas/practica02/Practica_2/codigo/src/main/java/testmaven/Test.java
@@ -4,8 +4,6 @@ import java.io.*;
 public class Test {
 
     public static void main (String[] args){
-        
-        
         AnalizadorLexico al = new AnalizadorLexico("src/main/resources/test.txt");
         al.analiza();
     }
