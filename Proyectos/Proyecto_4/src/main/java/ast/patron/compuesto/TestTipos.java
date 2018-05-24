7eec7acc94af19ab58b1d66a24a2007843815b65
Héctor Santaella Marín <santaella@ciencias.unam.mx>
2018-05-23 21:46:22 -0500

    Se agregaron algunas deifiniciones y el constructor TestTipos

--- Proyectos/Proyecto_4/src/main/java/ast/patron/compuesto/TestTipos.java
+++ Proyectos/Proyecto_4/src/main/java/ast/patron/compuesto/TestTipos.java
@@ -9,11 +9,21 @@ import java.util.ArrayList;
 
 /**
  *
- * @author juan
+ * @author juan_Ricky_Riquín_Canallín
  */
 public class TestTipos {
     
-    ArrayList<String> lista;
+    ArrayList<String> lista_tipos;
+    
+    
+    /*
+    Se define como:
+    0=booleano
+    1=entero
+    2=real
+    3=cadena
+    -1=invalido
+    */
   
     //Tabla de tipo para la resta
     private int[][] resta = new int[][]{{0,1,2,3},
@@ -30,9 +40,41 @@ public class TestTipos {
                                                  {1,1,2,-1},
                                                  {2,2,2,-1},
                                                  {3,-1,-1,-1}};
-    //Tabla de tipo 
-    private int[][] div = new int[][]{{1,1,2,-1},
+    //Tabla de tipo para la division
+    private int[][] div = new int[][]{
+                                      {1,1,2,-1},
                                       {1,1,2,-1},
                                       {2,2,2,-1},
                                       {-1,-1,-1,-1}};
+    
+    //Tabla de modulo
+    private int[][] mod= new int [][]{
+                                               {1, 1, 2, -1},
+                                               {1, 1, 2, -1},
+                                               {2, 2, 2, -1},
+                                               {-1,-1,-1,-1} };
+    
+    //Tabla de comparaciones
+    private int [][] com= new int[][]{
+                                        {0, 0, 0, 0},
+                                        {0, 0, 0, 0},
+                                        {0, 0, 0, 0},
+                                        {0, 0, 0, 0}};
+    
+    
+    
+    //Tabla para la operacion  unaria
+  private final byte[] unarios = new byte[] {1,1,2,-1};
+
+    
+    //Constructor
+    public TestTipos() {
+    lista_tipos = new ArrayList<String>();
+    lista_tipos.add("Booleano");
+    lista_tipos.add("Entero");
+    lista_tipos.add("Real");
+    lista_tipos.add("Cadena");
+  }
+
+
 }
